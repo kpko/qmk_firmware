8a37f30d952c0c119f9538276f54a60dd283ce5d
Kevin Preller <kevin.preller@wmit.biz>
2021-09-01 15:17:01 +0200

    Momoka ergo home row mods

---
+++ keyboards/momoka_ergo/keymaps/kpko/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define TAPPING_TERM 200 // Stuff for home row mods
+#define IGNORE_MOD_TAP_INTERRUPT // Prevent normal rollover on alphas from accidentally triggering mods.
+#define TAPPING_FORCE_HOLD // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define PERMISSIVE_HOLD // Apply the modifier on keys that are tapped during a short hold of a modtap
